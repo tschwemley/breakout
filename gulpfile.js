5896471e73dab4be1584da1fd255bc18073f0ce1
Tyler Schwemley <tjschwem@gmail.com>
2016-11-08 21:58:04 -0500

    Initial gulp config and dependency install

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,21 @@
 
 var gulp = require('gulp'),
     del = require('del'),
+    concat = require('gulp-concat'),
+    merge = require('merge-stream'),
 
     tsc  = require('gulp-typescript'),
     jsMin = require('gulp-uglify'),
     
     paths = {
+      vendor: [
+        'node_modules/phaser/dist/pixi.js',
+        'node_modules/phaser/dist/p2.js',
+        'node_modules/phaser/dist/phaser-creature.js',
+        'node_modules/systemjs/dist/system-polyfills.js',
+        'node_modules/systemjs/dist/system.js'
+      ],
+      ts: 'src/**/*.ts',
       html: 'src/**/*.html'
     };
 
@@ -23,4 +33,30 @@ gulp.task('tsc', () => {
 
   return tsResult.js.pipe(gulp.dest('dist/game/'));
 });
+
+gulp.task('vendor', () => {
+  return gulp.src(paths.vendor)
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('minify', () => {
+  let js = gulp.src('dist/**/*.js')
+      .pipe(jsMin())
+      .pipe(gulp.dest('dist/'));
+
+  return js;
+});
     
+gulp.task('watch', () => {
+  let watchHtml = gulp.watch(paths.html, ['html']),
+      watchTs   = gulp.watch(paths.ts, ['tsc']),
+
+      onChanged = (event) => console.log('File ' + event.path + ' was ' +
+          event.type + '. Running tasks...');
+
+  watchHtml.on('change', onChanged);
+  watchTs.on('change', onChanged);
+});
+
+gulp.task('default', ['html', 'tsc', 'vendor']);
